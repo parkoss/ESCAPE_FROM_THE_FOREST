723bd826cab331e9c566d76a47c177422f7fff42
parkoss <marek.sedlacek73@seznam.cz>
2025-02-26 18:58:16 +0100

    pridana trida Lokace, upraveny soubor map.txt, ve tride MapaLesa dodelana metoda pro nactiMapu

--- src/MapaLesa.java
+++ src/MapaLesa.java
@@ -1,19 +1,26 @@
-import javax.xml.stream.Location;
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class MapaLesa {
     private int start = 0;
     private int poloha=start;
+    private ArrayList<String> povolenyLokace=new ArrayList<>();
+    private ArrayList<Lokace>vsechnyLokace=new ArrayList<>();
+    Lokace lokace;
 
     public boolean nactiMapu(){
         try(BufferedReader br = new BufferedReader(new FileReader("mapa.txt"))){
             String line;
             while((line=br.readLine())!=null) {
-                String[] lines = line.split(";");
+                String[] lines = line.split("-");
+                for (int i = 0; i < lines.length; i++) {
+                    povolenyLokace.add(lines[i]);
+                }
+                lokace=new Lokace(lines[0],povolenyLokace);
+                vsechnyLokace.add(lokace);
+
             }
 
 
