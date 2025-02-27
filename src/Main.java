cf46166a71b78b888340c36839de74b6bd63cef8
parkoss <marek.sedlacek73@seznam.cz>
2025-02-27 22:50:54 +0100

    pridana trida pro pohyb pres soubory

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        MapaLesa mapaLesa= new MapaLesa();
+        new Hra().start();
         
     }
 }
--- src/MapaLesa.java
+++ src/MapaLesa.java
@@ -10,8 +10,6 @@ public class MapaLesa {
     private ArrayList<String> povolenyLokace=new ArrayList<>();
     private ArrayList<Lokace>vsechnyLokace=new ArrayList<>();
     Lokace lokace;
-    private HashMap<String, Lokace> lokaceMapa = new HashMap<>();
-    private String aktualniLokace;
 
     public boolean nactiMapu(){
         try(BufferedReader br = new BufferedReader(new FileReader("mapa.txt"))){
@@ -22,27 +20,13 @@ public class MapaLesa {
                 for (int i = 0; i < lines.length; i++) {
                     povolenyLokace.add(lines[i]);
                 }
-                lokaceMapa.put(nazevLokace, new Lokace(nazevLokace, povolenyLokace));
                 lokace=new Lokace(lines[0],povolenyLokace);
                 vsechnyLokace.add(lokace);
-                lokaceMapa.put(lokace.getNazev(), new Lokace(lokace.getNazev(), povolenyLokace));
             }
-            aktualniLokace="startovniKemp";
             return true;
 
         }catch (IOException e){
             return false;
         }
-    }
-    public Lokace getLokace(String nazev) {
-        return lokaceMapa.get(nazev);
-    }
-
-    public String getAktualniLokace() {
-        return aktualniLokace;
-    }
-
-    public void setAktualniLokace(String novaLokace) {
-        aktualniLokace = novaLokace;
     }
 }
